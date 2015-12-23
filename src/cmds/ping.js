818f42b1313e14d36c147776fbeb36501deea66d
Dexon <dexon@live.ca>
2015-12-23 07:07:56 +0000

    update

--- src/cmds/ping.js
+++ src/cmds/ping.js
@@ -3,8 +3,10 @@ module.exports = {
     exec: function(data){
         if(typeof data === "undefined") return;
         var username = data.username,
-            channelName = data.channelName;
+            channelName = data.channelName,
+            messageDate = Date.parse(data.msg.date),
+            now = new Date().getTime();
         
-        require("../bot.js").dexonbot.webClient.doSay("pong", channelName);
+        require("../bot.js").dexonbot.webClient.doSay("pong ("+(now - messageDate)+" ms)", channelName);
     }
 }
