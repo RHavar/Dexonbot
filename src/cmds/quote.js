c4302483b28cdc261bbcedbd08db82a211e50ab2
Dexon <dexon@live.ca>
2015-12-23 06:15:32 +0000

    update

---
+++ src/cmds/quote.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "quote",
+	exec: function(data){
+		if(typeof data === "undefined") return;
+		var username = data.username,
+			channelName = data.channelName;
+        
+        
+	}
+}
