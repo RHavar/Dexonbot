44990bb1a81ee2feb7ee625e271cc365ca2a0c50
Dexon <dexon@live.ca>
2015-12-23 00:24:31 -0500

    update

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,7 @@
+var sensitive = require('../../sensitive_data/data');
 module.exports = {
     GAMESERVER: "https://gs.bustabit.com",
     WEBSERVER: "https://www.bustabit.com",
-    SESSION: process.env.BOT_SESSION || "",
-    PASSWORD: process.env.BOT_PASSWORD || ""
+    SESSION: process.env.BOT_SESSION || sensitive.BOT_SESSION || "",
+    PASSWORD: process.env.BOT_PASSWORD || sensitive.BOT_PASSWORD || ""
 };
