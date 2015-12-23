6202ba2c3f5df84a5e60a2a099316ec74ea8bb1d
Dexon <dexon@live.ca>
2015-12-23 00:07:57 -0500

    update

--- src/bot.js
+++ src/bot.js
@@ -51,6 +51,19 @@ function DexonBot(){
 }
 var dexonbot = new Dexonbot();
 
+/* SERVER
+-----------------*/
+var express = require('express'),
+    http = require('http'),
+    app = express();
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
+
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+});
+
 /* UNCAUGHT EXCEPTIONS
 -----------------*/
 process.on('uncaughtException', function(err) {
